657514b2f5c589870cc4d6ca06c4bce103153a02
Florin Hasna <florinhasna@outlook.com>
2024-01-19 09:57:47 +0000

    Created test assertions for power function.

--- maths_tests.cpp
+++ maths_tests.cpp
@@ -13,3 +13,15 @@ TEST_CASE("test primes", "[is_prime]")
   REQUIRE(is_prime(8) == false);
   REQUIRE(is_prime(9) == false);
 }
+
+TEST_CASE("test powers", "[power]")
+{
+  REQUIRE(power(2, 2) == 4);
+  REQUIRE(power(2, 3) == 8);
+  REQUIRE(power(2, 4) == 16);
+  REQUIRE(power(3, 2) == 9);
+  REQUIRE(power(3, 3) == 27);
+  REQUIRE(power(5, 2) == 25);
+  REQUIRE(power(5, 3) == 125);
+  REQUIRE(power(10, 2) == 100);
+}
